59ab5c97a739df00a29522540bb554fd18367236
Chris French <chris@ammonitenetworks.com>
2019-07-24 16:58:50 -0700

    going to dinner

---
+++ app/Job.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    
+
+	public function applicants() {
+
+		return $this->hasMany('App\Applicant');
+	}
+
+
+}
